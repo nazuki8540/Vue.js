7195ef34cff29a0f40783b091762c8872ef51e47
Matheus Ghotme <51427002+nazuki8540@users.noreply.github.com>
2022-09-20 23:34:49 -0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <h1>Guia Clientes</h1>
+    <Cliente/>
+    <Cliente/>
+    <Cliente/>
+    <Cliente/>
+      
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import Cliente from './components/Cliente'
+export default {
+  name: 'App',
+  components: {
+    Cliente
   }
 }
+</script>
+
+<style>
 </style>
---
+++ src/components/Cliente.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="cliente">
+        <h2 id="cliente-nome">Cliente aqui! - Componente aqui</h2>
+        <h3>Teste</h3>
+        <hr>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    #cliente {
+        color: green;
+        background-color: black;
+    }
+    
+    #cliente-nome {
+        color: white;
+    }
+</style>
