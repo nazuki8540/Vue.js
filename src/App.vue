fcb521063fc33fc338f25e361692b378afb3baab
Matheus Ghotme <51427002+nazuki8540@users.noreply.github.com>
2022-09-27 19:16:02 -0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
     <input type="text" v-model="clienteVictor.nome">
-    <Cliente :cliente="clienteVictor" :showIdade="true" />
-    <Cliente :cliente="clienteVictor" :showIdade="false"/>
-    <Cliente :cliente="clienteVictor" :showIdade="true" />
-    <Cliente :cliente="clienteVictor" :showIdade="false"/>
-
-    <Produto/>
     
+    <div v-for="(cliente,index) in clientes" :key="cliente.id">
+      <h4>{{ index + 1}}</h4>
+      <Cliente :cliente="cliente" />
+      <hr>
+      <h4>Edição:</h4>
+      <input type="text" v-model="cliente.nome">
+      <input type="text" v-model="cliente.email">
+    </div>
    
   </div>
 </template>
@@ -24,7 +26,33 @@ export default {
         nome: "victor lima",
         email: "victor@gmail.com",
         idade: 2
-      }
+      },
+      clientes: [
+        {
+          id: 2,
+          nome:"matheus v",
+          email: "amthedus@gam.acom",
+          idade: 99
+        },
+        {
+          id: 5,
+          nome:"matheus ghotme",
+          email: "amtheus3@gam.acom",
+          idade: 16
+        },
+        {
+          id: 3,
+          nome:"matheus voltz",
+          email: "amth1eus@gam.acom",
+          idade: 26
+        },
+        {
+          id: 7,
+          nome:"matheus voltzasda",
+          email: "amtaheus@gam.acom",
+          idade: 26
+        }
+      ]
     }
   },
   components: {
