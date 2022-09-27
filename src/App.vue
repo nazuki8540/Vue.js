225bf02003595700d0fc5adfddcb2502517ce073
Matheus Ghotme <51427002+nazuki8540@users.noreply.github.com>
2022-09-26 22:20:28 -0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
     <h1>Guia Clientes</h1>
-    <Cliente/>
-    <Cliente/>
-    <Cliente/>
-    <Cliente/>
-    <hr>
-      <h4>Lista de Produtos!</h4>
-    <hr>
+    <Cliente :cliente="clienteVictor" />
+    <Cliente nome="Matheus"  email="matheu2s@gmail.com" idade="2"/>
+    <Cliente nome="Matheus"  email="mat2heus@gmail.com" idade="4"/>
+    <Cliente nome="Matheus" email="mat22heus@gmail.com" idade="54"/>
+
     <Produto/>
-    <Produto/>
-    <Produto/>
+    
+   
   </div>
 </template>
 
 <script>
 import Cliente from './components/Cliente';
-import Produto from './components/Produto';
+// import Produto from './components/Produto';
 export default {
   name: 'App',
+  data(){
+    return{
+      
+      clienteVictor: {
+        nome: "victor lima",
+        email: "victor@gmail.com",
+        idade: 99
+      }
+    }
+  },
   components: {
     Cliente,
-    Produto,
-    Produto
+    // Produto,
+    // Produto
 }
 }
 </script>
--- src/components/Cliente.vue
+++ src/components/Cliente.vue
@@ -1,24 +1,38 @@
 <template>
     <div id="cliente">
-        <h2 id="cliente-nome">Cliente aqui! - Componente aqui</h2>
-        <h3>Teste</h3>
-        <hr>
+       <h4>Nome: {{cliente.nome}}</h4>
+       <hr>
+       <p>Email: {{cliente.email}}</p>
+       <p>Idade: {{cliente.idade}}</p>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data(){
+        return {
+        }
+    },
+    props: {
+        nome: String,
+        email: String,
+        idade: Number,
+        cliente: Object
+    }
 }
 </script>
 
 <style scoped>
     #cliente {
+        background-color: #ECE5E3;
         color: green;
-        background-color: black;
+        max-width: 600px;
+        height: 180px;
+        padding: 1%;
+        margin-top: 2%;
     }
     
     #cliente-nome {
-        color: white;
+        color: black;
     }
 </style>
