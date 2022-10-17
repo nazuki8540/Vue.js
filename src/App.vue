7e6e3a9fe0debd9253031719303f749ea377f616
Matheus Ghotme <51427002+nazuki8540@users.noreply.github.com>
2022-10-17 20:07:33 -0300

    bulma

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+
+
+    <div class="buttons">
+      <button class="button is-primary">Primary</button>
+      <button class="button is-link">link</button>
+    </div>
+
     <h3>Cadastro:</h3>
     <input type="text" placeholder="nome" v-model="nomeField"><br>
     <small id="nomeErro" v-show="deuErro">O nome é invalido ! Tente novamente</small> <br>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import { createApp, h } from 'vue';
-import App from './App.vue';
+import Vue from 'vue'
+import App from './App.vue'
 import router from './router';
 import store from './store';
 import "../node_modules/bulma/css/bulma.css"; 
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-    render: r=> h(App),
+    render: h => h(App),
 }).$mount('#app')
 
 createApp(App).use(store).use(router).mount('#app');
