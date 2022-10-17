ee73e735f833bb790d0fb1e4cc95231c3324d543
Matheus Ghotme <51427002+nazuki8540@users.noreply.github.com>
2022-10-17 19:54:00 -0300

    bulma

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import { createApp } from 'vue';
+import { createApp, h } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import "../node_modules/bulma/css/bulma.css"; 
+
+Vue.config.productionTip = false
+
+new Vue({
+    render: r=> h(App),
+}).$mount('#app')
 
 createApp(App).use(store).use(router).mount('#app');
